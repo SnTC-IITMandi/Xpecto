e6800aed47772c2e7d8066320e0bfa25ae120e9a
Sarvesh Jadhav <sarveshjadhav543@gmail.com>
2023-02-24 13:07:54 +0530

    payment details moved to db

--- server/client/src/pages/public/Payment/Payment.jsx
+++ server/client/src/pages/public/Payment/Payment.jsx
@@ -1,64 +1,115 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./Payment.module.css";
 import Layout from "../component/Layout/Layout";
+import { ThreeCircles } from "react-loader-spinner";
+import axios from "axios";
 
 const Payment = () => {
+  const [paymentDetails, setPaymentDetails] = useState(null);
+  const deadline = useMemo(() => {
+    if (!paymentDetails) return null;
+    return new Date(paymentDetails.deadline);
+  }, [paymentDetails]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // fire window scroll event to update scrollbar
+    window.dispatchEvent(new Event("scroll"));
+  }, [paymentDetails]);
+
+  const getPaymentDetails = async () => {
+    try {
+      const url = `${process.env.REACT_APP_BACKENDURL}/api/payment/details`;
+      const data = await axios.get(url);
+
+      setPaymentDetails((prev) => data.data.data);
+      // console.log();
+      setIsLoading((prev) => false);
+      // const teamsObj = {};
+    } catch (err) {
+      // setFaqs(tempFaqs);
+      setIsLoading((prev) => true);
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getPaymentDetails();
+  }, []);
+
   return (
     <>
       <Layout dataColor="#00ddcc">
         <div className={styles["header"]}>
           <h1 className={styles["events-page-heading"]}>Payment</h1>
-          <div className={styles["payment-main"]}>
-            <div className={styles["content-container"]}>
-              <div className={styles["message"]}>
-                <h1 className={styles["amount"]}>Rs. 1700 per person</h1>
-                <h1>For offline participation</h1>
-                <h2>
-                  INCLUDED <br />3 days Accomodation + Food + Event
-                  Participation + Pro Night
-                </h2>
-                <h1 className={styles["payment-deadline"]}>
-                  Deadline for fee payment
-                  <br />
-                  25 Feb, 2023
-                </h1>
-                {/* <h2>Payment to be made by CONTINGENT LEADER ONLY</h2> */}
-                <h2>Please fill the form after Payment</h2>
-                {/* <h2></h2> */}
-              </div>
-              <div className={styles["content-inner"]}>
-                <a
-                  href="https://forms.gle/LMstcRnCQwbfcsTw7"
-                  target="_blank"
-                  className={styles["form-link"]}
-                >
-                  FORM
-                </a>
-                <div className={styles["info"]}>
-                  <div className={styles["account-name"]}>
-                    <span>Account Name</span>
-                    <span>SCIENCE AND TECHNOLOGY COUNCIL</span>
-                  </div>
-                  <div className={styles["account-name"]}>
-                    <span>Bank Name</span>
-                    <span>PUNJAB NATIONAL BANK</span>
-                  </div>
-                  <div className={styles["account-name"]}>
-                    <span>Bank Branch</span>
-                    <span>IIT KAMAND, KAMAND, MANDI, HP BRANCH</span>
-                  </div>
-                  <div className={styles["account-number"]}>
-                    <span>Account Number</span>
-                    <span>7315000100034536</span>
-                  </div>
-                  <div className={styles["ifsc-code"]}>
-                    <span>IFSC Code</span>
-                    <span>PUNB0731500</span>
+
+          {isLoading ? (
+            <div className={styles["loading"]}>
+              <ThreeCircles
+                height="150px"
+                width="150px"
+                color="#fff"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="three-circles-rotating"
+                outerCircleColor=""
+                innerCircleColor="#00ddcc"
+                middleCircleColor=""
+              />
+            </div>
+          ) : !paymentDetails ? null : (
+            <div className={styles["payment-main"]}>
+              <div className={styles["content-container"]}>
+                <div className={styles["message"]}>
+                  <h1 className={styles["amount"]}>Rs. {paymentDetails.amount} per person</h1>
+                  <h1>For offline participation</h1>
+                  <h2>
+                    INCLUDED <br />{paymentDetails.includes}
+                  </h2>
+                  <h1 className={styles["payment-deadline"]}>
+                    Deadline for fee payment
+                    <br />
+                    {`${deadline.getDate()} ${deadline.toLocaleString("en-us", {month:"short"})}, ${deadline.getFullYear()}`}
+                  </h1>
+                  {/* <h2>Payment to be made by CONTINGENT LEADER ONLY</h2> */}
+                  <h2>Please fill the form after Payment</h2>
+                  {/* <h2></h2> */}
+                </div>
+                <div className={styles["content-inner"]}>
+                  <a
+                    href={paymentDetails.formLink}
+                    target="_blank"
+                    className={styles["form-link"]}
+                  >
+                    FORM
+                  </a>
+                  <div className={styles["info"]}>
+                    <div className={styles["account-name"]}>
+                      <span>Account Name</span>
+                      <span>{paymentDetails.accountName}</span>
+                    </div>
+                    <div className={styles["account-name"]}>
+                      <span>Bank Name</span>
+                      <span>{paymentDetails.bankName}</span>
+                    </div>
+                    <div className={styles["account-name"]}>
+                      <span>Bank Branch</span>
+                      <span>{paymentDetails.bankBranch}</span>
+                    </div>
+                    <div className={styles["account-number"]}>
+                      <span>Account Number</span>
+                      <span>{paymentDetails.accountNumber}</span>
+                    </div>
+                    <div className={styles["ifsc-code"]}>
+                      <span>IFSC Code</span>
+                      <span>{paymentDetails.ifscCode}</span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </Layout>
     </>
